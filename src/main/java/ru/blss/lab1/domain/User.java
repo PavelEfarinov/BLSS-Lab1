d230dc44ea965dc99aaafd44339714190ae921ae
PavelEfarinov <pavelefarinov@gmail.com>
2021-03-08 19:33:27 +0300

    removed some user fields

--- src/main/java/ru/blss/lab1/domain/User.java
+++ src/main/java/ru/blss/lab1/domain/User.java
@@ -1,13 +1,10 @@
 package ru.blss.lab1.domain;
 
-import org.hibernate.annotations.CreationTimestamp;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-import java.util.Date;
 
 @SuppressWarnings({"unused", "WeakerAccess"})
 @Entity
@@ -38,18 +35,6 @@ public class User {
     @Size(min = 1, max = 100)
     @Pattern(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
     private String email;
-
-    @NotNull
-    @NotEmpty
-    @Size(min = 1, max = 100)
-    private String organization;
-
-    @CreationTimestamp
-    private Date creationTime;
-
-    private double rating;
-
-    private boolean admin;
 
     public long getId() {
         return id;
@@ -89,13 +74,5 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public Date getCreationTime() {
-        return creationTime;
-    }
-
-    public void setCreationTime(Date creationTime) {
-        this.creationTime = creationTime;
     }
 }
