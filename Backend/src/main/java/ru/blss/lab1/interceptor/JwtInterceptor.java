20a6ebf3563e3d9aefca82b1991513018cf9171e
PavelEfarinov <pavelefarinov@gmail.com>
2021-03-12 11:25:28 +0300

    fixed token validation

--- Backend/src/main/java/ru/blss/lab1/interceptor/JwtInterceptor.java
+++ Backend/src/main/java/ru/blss/lab1/interceptor/JwtInterceptor.java
@@ -5,11 +5,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import ru.blss.lab1.controller.ApiController;
+import ru.blss.lab1.domain.User;
+import ru.blss.lab1.exception.ValidationException;
 import ru.blss.lab1.service.JwtService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
+import java.util.Optional;
 
 @Component
 public class JwtInterceptor implements HandlerInterceptor {
@@ -24,6 +27,7 @@ public class JwtInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
         if (handler instanceof HandlerMethod) {
             HandlerMethod handlerMethod = (HandlerMethod) handler;
             Method method = handlerMethod.getMethod();
@@ -32,7 +36,14 @@ public class JwtInterceptor implements HandlerInterceptor {
                 String authorization = request.getHeader("Authorization");
                 if (authorization != null && authorization.startsWith(BEARER)) {
                     String token = authorization.substring(BEARER.length()).trim();
-                    jwtService.find(token).ifPresent(user -> request.setAttribute("user", user));
+                    Optional<User> user = jwtService.find(token);
+                    if(user.isPresent()){
+                        request.setAttribute("user", user.get());
+                    }
+                    else
+                    {
+                        throw new ValidationException("Invalid user token");
+                    }
                 }
             }
         }
