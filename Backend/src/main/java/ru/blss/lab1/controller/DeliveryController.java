d4398b8819644aeeedb22a2f47b76c41095b682b
Ilya Ivanov <ilya_ivanov_777@mail.ru>
2021-03-21 00:59:23 +0300

    create DeliveryController (no test), fix DeliveryService

---
+++ Backend/src/main/java/ru/blss/lab1/controller/DeliveryController.java
@@ -0,0 +1,105 @@
+package ru.blss.lab1.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.blss.lab1.domain.*;
+import ru.blss.lab1.service.DeliveryService;
+
+import java.sql.Timestamp;
+
+@RestController
+@RequestMapping("/api/1")
+public class DeliveryController {
+    private DeliveryService deliveryService;
+
+    public DeliveryController(DeliveryService deliveryService) {
+        this.deliveryService = deliveryService;
+    }
+
+    @PostMapping("courier")
+    public void upgradeToCourierRole(@RequestBody User user) {
+        deliveryService.giveCourierRole(user);
+    }
+
+    @PostMapping("courier/orders/choose")
+    public void chooseOrder(long courierId, long orderId) {
+        deliveryService.setOrderCourier(courierId, orderId);
+    }
+
+    @PostMapping("orders/new")
+    public void addNewOrder(@RequestBody OrderInfoDTO orderInfo) {
+        deliveryService.addNewOrder(orderInfo.getAddress(), orderInfo.getPaymentStatus());
+    }
+
+    @PostMapping("orders/update/status")
+    public void updateOrderStatus(long id, String status) {
+        deliveryService.updateOrderStatus(id, status);
+    }
+
+    @PostMapping("courier/flight/new")
+    public void addNewCarFlight(@RequestBody FlightInfoDTO flightInfo) {
+        deliveryService.addDeliveryCarFlight(flightInfo.getBegin(), flightInfo.getEnd(), flightInfo.getCourierId());
+    }
+}
+
+class OrderInfoDTO{
+    private String address;
+    private String orderStatus;
+    private String paymentStatus;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+}
+
+class FlightInfoDTO {
+    private long courierId;
+    private Timestamp begin;
+    private Timestamp end;
+
+    public long getCourierId() {
+        return courierId;
+    }
+
+    public void setCourierId(long courierId) {
+        this.courierId = courierId;
+    }
+
+    public Timestamp getBegin() {
+        return begin;
+    }
+
+    public void setBegin(Timestamp begin) {
+        this.begin = begin;
+    }
+
+    public Timestamp getEnd() {
+        return end;
+    }
+
+    public void setEnd(Timestamp end) {
+        this.end = end;
+    }
+}
--- Backend/src/main/java/ru/blss/lab1/service/DeliveryService.java
+++ Backend/src/main/java/ru/blss/lab1/service/DeliveryService.java
@@ -3,43 +3,52 @@ package ru.blss.lab1.service;
 import org.springframework.stereotype.Service;
 import ru.blss.lab1.domain.Courier;
 import ru.blss.lab1.domain.DeliveryCarFlight;
+import ru.blss.lab1.domain.Orders;
 import ru.blss.lab1.domain.User;
 import ru.blss.lab1.repository.CourierRepository;
 import ru.blss.lab1.repository.DeliveryCarRepository;
+import ru.blss.lab1.repository.OrderRepository;
 
-import java.sql.Date;
 import java.sql.Timestamp;
 
 @Service
 public class DeliveryService {
     private CourierRepository courierRepository;
     private DeliveryCarRepository deliveryCarRepository;
+    private OrderRepository orderRepository;
 
-    public DeliveryService(CourierRepository courierRepository, DeliveryCarRepository deliveryCarRepository) {
+    public DeliveryService(CourierRepository courierRepository, DeliveryCarRepository deliveryCarRepository, OrderRepository orderRepository) {
         this.courierRepository = courierRepository;
         this.deliveryCarRepository = deliveryCarRepository;
+        this.orderRepository = orderRepository;
     }
 
-    public void addDeliveryInfo(String address, Date date, Courier courier) {
-        courierRepository.addAddress(courier.getId(), address);
-        courierRepository.addDeliveryDate(courier.getId(), date);
-        courierRepository.save(courier);
+    public void addNewOrder(String address, String paymentStatus){
+        Orders order = new Orders();
+        order.setAddress(address);
+        order.setPaymentStatus(paymentStatus);
+        orderRepository.save(order);
     }
 
-    public void setCourierStatus(String status, Courier courier) {
-        courierRepository.changeStatus(courier.getId(), status);
-        courierRepository.save(courier);
+    public void setOrderCourier(long courierId, long orderId){
+        Orders order = orderRepository.getOne(orderId);
+        if (order.getCourier().getId() != null)
+            orderRepository.setCourier(orderId, courierId);
     }
 
-    public void addDeliveryCarFlight(Timestamp begin, Timestamp end, Courier courier){
+    public void updateOrderStatus(long orderId, String status){
+        orderRepository.updateOrderStatus(orderId, status);
+    }
+
+    public void addDeliveryCarFlight(Timestamp begin, Timestamp end, long courierId){
         DeliveryCarFlight deliveryCar = new DeliveryCarFlight();
-        deliveryCar.setId(courier.getId());
-        deliveryCarRepository.changeArrivalTime(deliveryCar.getId(), begin);
-        deliveryCarRepository.changeDepartureTime(deliveryCar.getId(), end);
+        deliveryCar.setId(courierId);
+        deliveryCarRepository.updateArrivalTime(deliveryCar.getId(), begin);
+        deliveryCarRepository.updateDepartureTime(deliveryCar.getId(), end);
         deliveryCarRepository.save(deliveryCar);
     }
 
-    public void giveCourierAccount(User user){
+    public void giveCourierRole(User user){
         Courier courier = new Courier();
         courier.setId(user.getId());
         courierRepository.save(courier);
