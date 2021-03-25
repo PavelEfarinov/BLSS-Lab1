55e8279f1f399ae360ba29438691cb29591a4cb0
Ilya Ivanov <ilya_ivanov_777@mail.ru>
2021-03-25 21:08:14 +0300

    fix methods in DeliveryService

--- Backend/src/main/java/ru/blss/lab1/service/DeliveryService.java
+++ Backend/src/main/java/ru/blss/lab1/service/DeliveryService.java
@@ -1,38 +1,46 @@
 package ru.blss.lab1.service;
 
 import org.springframework.stereotype.Service;
-import ru.blss.lab1.domain.Courier;
-import ru.blss.lab1.domain.DeliveryCarFlight;
-import ru.blss.lab1.domain.Orders;
-import ru.blss.lab1.domain.User;
-import ru.blss.lab1.repository.CourierRepository;
-import ru.blss.lab1.repository.DeliveryCarRepository;
-import ru.blss.lab1.repository.OrderRepository;
+import ru.blss.lab1.domain.*;
+import ru.blss.lab1.repository.*;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 @Service
 public class DeliveryService {
     private CourierRepository courierRepository;
     private DeliveryCarRepository deliveryCarRepository;
     private OrderRepository orderRepository;
+    private OrderItemRepository orderItemRepository;
+    private CartItemRepository cartItemRepository;
 
-    public DeliveryService(CourierRepository courierRepository, DeliveryCarRepository deliveryCarRepository, OrderRepository orderRepository) {
+    public DeliveryService(CourierRepository courierRepository, DeliveryCarRepository deliveryCarRepository, OrderRepository orderRepository, OrderItemRepository orderItemRepository, CartItemRepository cartItemRepository) {
         this.courierRepository = courierRepository;
         this.deliveryCarRepository = deliveryCarRepository;
         this.orderRepository = orderRepository;
+        this.orderItemRepository = orderItemRepository;
+        this.cartItemRepository = cartItemRepository;
     }
 
-    public void addNewOrder(String address, String paymentStatus){
+    public void addNewOrder(User user, String address, String paymentStatus){
         Orders order = new Orders();
-        order.setAddress(address);
-        order.setPaymentStatus(paymentStatus);
-        orderRepository.save(order);
+        List<StoreItemInCart> storeItemInCarts = cartItemRepository.getCartItemByOwnerId(user.getId());
+
+        if (!storeItemInCarts.isEmpty()) {
+            order.setAddress(address);
+            order.setPaymentStatus(paymentStatus);
+            order.setClient(user);
+            orderRepository.save(order);
+            for (StoreItemInCart item : storeItemInCarts) {
+                orderItemRepository.save(new OrderItems(item.getQuantity(), item.getItem(), order));
+            }
+        }
     }
 
     public void setOrderCourier(long courierId, long orderId){
         Orders order = orderRepository.getOne(orderId);
-        if (order.getCourier().getId() != null)
+        if (order.getCourier() == null)
             orderRepository.setCourier(orderId, courierId);
     }
 
@@ -40,12 +48,22 @@ public class DeliveryService {
         orderRepository.updateOrderStatus(orderId, status);
     }
 
-    public void addDeliveryCarFlight(Timestamp begin, Timestamp end, long courierId){
+    public void addDeliveryCarFlight(Timestamp begin, Timestamp end, long courierId, String address){
         DeliveryCarFlight deliveryCar = new DeliveryCarFlight();
-        deliveryCar.setId(courierId);
-        deliveryCarRepository.updateArrivalTime(deliveryCar.getId(), begin);
-        deliveryCarRepository.updateDepartureTime(deliveryCar.getId(), end);
-        deliveryCarRepository.save(deliveryCar);
+        Courier courier = courierRepository.getOne(courierId);
+        List<Orders> orders = orderRepository.getAllByCourierId(courierId);
+        if (courier != null && orders != null)
+        {
+            deliveryCar.setCourier(courier);
+
+            deliveryCar.setArrivalTime(begin);
+            deliveryCar.setDepartureTime(end);
+
+            for (Orders order: orders) {
+                orderRepository.updateAddress(order.getId(), address);
+            }
+            deliveryCarRepository.save(deliveryCar);
+        }
     }
 
     public void giveCourierRole(User user){
