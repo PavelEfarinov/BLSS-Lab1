f52a63b01d3e4ea3d88627e1f1a4d236690cae8c
PavelEfarinov <pavelefarinov@gmail.com>
2021-03-25 23:02:55 +0300

    splitted contollers

---
+++ Backend/src/main/java/ru/blss/lab1/controller/CourierController.java
@@ -0,0 +1,67 @@
+package ru.blss.lab1.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.blss.lab1.domain.Orders;
+import ru.blss.lab1.exception.UnauthorizedUserException;
+import ru.blss.lab1.service.DeliveryService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.Timestamp;
+
+@RestController
+@RequestMapping("/api/1")
+public class CourierController extends ApiController {
+    private DeliveryService deliveryService;
+
+    public CourierController(DeliveryService deliveryService) {
+        this.deliveryService = deliveryService;
+    }
+
+    @PostMapping("courier")
+    public void upgradeToCourierRole(HttpServletRequest request) throws UnauthorizedUserException {
+        deliveryService.giveCourierRole(getUser(request));
+    }
+
+    @PostMapping("courier/orders/choose")
+    public void chooseOrder(@RequestBody Orders orderInfo, HttpServletRequest request) {
+        deliveryService.setOrderCourier(getUser(request).getId(), orderInfo.getId());
+    }
+
+    @PostMapping("courier/flight/new")
+    public void addNewCarFlight(@RequestBody FlightInfoDTO flightInfo, HttpServletRequest request) {
+        deliveryService.addDeliveryCarFlight(flightInfo.getBegin(), flightInfo.getEnd(), getUser(request).getId(), flightInfo.getAddress());
+    }
+
+    class FlightInfoDTO {
+        private String address;
+        private Timestamp begin;
+        private Timestamp end;
+
+        public Timestamp getBegin() {
+            return begin;
+        }
+
+        public void setBegin(Timestamp begin) {
+            this.begin = begin;
+        }
+
+        public Timestamp getEnd() {
+            return end;
+        }
+
+        public void setEnd(Timestamp end) {
+            this.end = end;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+    }
+}
--- Backend/src/main/java/ru/blss/lab1/controller/DeliveryController.java
+++ Backend/src/main/java/ru/blss/lab1/controller/DeliveryController.java
@@ -1,135 +1,31 @@
 package ru.blss.lab1.controller;
 
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.blss.lab1.domain.*;
-import ru.blss.lab1.exception.UnauthorizedUserException;
+import ru.blss.lab1.domain.Orders;
 import ru.blss.lab1.service.DeliveryService;
 
 import javax.servlet.http.HttpServletRequest;
-import java.sql.Timestamp;
 
 @RestController
 @RequestMapping("/api/1")
-public class DeliveryController extends ApiController{
+public class DeliveryController extends ApiController {
     private DeliveryService deliveryService;
 
     public DeliveryController(DeliveryService deliveryService) {
         this.deliveryService = deliveryService;
     }
 
-    @PostMapping("courier")
-    public void upgradeToCourierRole(HttpServletRequest request) throws UnauthorizedUserException {
-        deliveryService.giveCourierRole(getUser(request));
-    }
-
-    @PostMapping("courier/orders/choose")
-    public void chooseOrder(@RequestBody OrderInfoDTO orderInfo) {
-        deliveryService.setOrderCourier(orderInfo.getCourierId(), orderInfo.getOrderId());
-    }
-
     @PostMapping("orders/new")
-    public void addNewOrder(@RequestBody OrderInfoDTO orderInfo, HttpServletRequest request) {
+    public void addNewOrder(@RequestBody Orders orderInfo, HttpServletRequest request) {
         deliveryService.addNewOrder(getUser(request), orderInfo.getAddress(), orderInfo.getPaymentStatus());
     }
 
     @PostMapping("orders/update/status")
-    public void updateOrderStatus(@RequestBody OrderInfoDTO orderUpdate) {
-        deliveryService.updateOrderStatus(orderUpdate.getOrderId(), orderUpdate.getOrderStatus());
+    public void updateOrderStatus(@RequestBody Orders orderUpdate) {
+        deliveryService.updateOrderStatus(orderUpdate.getId(), orderUpdate.getOrderStatus());
     }
 
-    @PostMapping("courier/flight/new")
-    public void addNewCarFlight(@RequestBody FlightInfoDTO flightInfo) {
-        deliveryService.addDeliveryCarFlight(flightInfo.getBegin(), flightInfo.getEnd(), flightInfo.getCourierId(), flightInfo.getAddress());
-    }
-}
-
-class OrderInfoDTO{
-    private long orderId;
-    private long courierId;
-    private String address;
-    private String orderStatus;
-    private String paymentStatus;
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getOrderStatus() {
-        return orderStatus;
-    }
-
-    public void setOrderStatus(String orderStatus) {
-        this.orderStatus = orderStatus;
-    }
-
-    public String getPaymentStatus() {
-        return paymentStatus;
-    }
-
-    public void setPaymentStatus(String paymentStatus) {
-        this.paymentStatus = paymentStatus;
-    }
-
-    public long getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(long orderId) {
-        this.orderId = orderId;
-    }
-
-    public long getCourierId() {
-        return courierId;
-    }
-
-    public void setCourierId(long courierId) {
-        this.courierId = courierId;
-    }
-}
-
-class FlightInfoDTO {
-    private long courierId;
-    private String address;
-    private Timestamp begin;
-    private Timestamp end;
-
-    public long getCourierId() {
-        return courierId;
-    }
-
-    public void setCourierId(long courierId) {
-        this.courierId = courierId;
-    }
-
-    public Timestamp getBegin() {
-        return begin;
-    }
-
-    public void setBegin(Timestamp begin) {
-        this.begin = begin;
-    }
-
-    public Timestamp getEnd() {
-        return end;
-    }
-
-    public void setEnd(Timestamp end) {
-        this.end = end;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
